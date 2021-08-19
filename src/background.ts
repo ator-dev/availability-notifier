932a1b2130be6d3a1bbde13d5312e09962aa0db2
Dominic-ATOR <dom@me>
2021-08-19 10:48:37 +0100

    Add availability checking to the background script

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,16 @@
 namespace background {
-	
+	function onLoad() {
+		const text = this.responseText;
+		for (const productId of ["310-458", "366-662"]) {
+			const i = text.indexOf('"StockStatus"', text.indexOf(`"${productId}"`));
+			const inStock = text.substring(i, i + 23) === '"StockStatus":"InStock"';
+			console.log(`Product ${productId} in stock: ${inStock}`);
+		}
+	}
+
+	const productKey = "ST007579";
+	const request = new XMLHttpRequest();
+	request.addEventListener("load", onLoad);
+	request.open("GET", `https://www.next.co.uk/style/${productKey}`);
+	request.send();
 }
